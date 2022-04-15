f2c518511d352db12b9d7c53a3045a816c47411f
philippreinig <philipp.reinig@gmail.com>
2022-04-15 22:40:59 +0200

    Added Sheet 1: 6 - Memory leak

---
+++ exercises/sheet_1/error.c
@@ -0,0 +1,25 @@
+/* gcc -w -o error error.c */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This function returns the address of the local variable b, which is invalid after exiting the function because it 
+	is stored on the stack and thus it's memory is cleared. */
+
+int *f(int a) {
+	int* b = malloc(sizeof(int));
+	*b = 2 * a; 
+	return b;
+}
+
+
+int main(void) {
+	int *p4, *p8; 
+	p4 = f(4); 
+	p8 = f(8); 
+	printf("p4: %i / p8: %i\n", *p4, *p8);
+	free(p4);
+	free(p8);
+}
