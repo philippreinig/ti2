14f12d02651c4fa67718b0af7fb522313c6d62be
philippreinig <philipp.reinig@gmail.com>
2022-04-22 11:13:49 +0200

    Fixed substr

--- exercises/sheet_1/substr.c
+++ exercises/sheet_1/substr.c
@@ -6,7 +6,7 @@
 
 /**
  * @brief Prints a messages and exists with failure code
- * 
+ *
  * @param serror Error message
  */
 static void panic(const char *serror)
@@ -17,18 +17,20 @@ static void panic(const char *serror)
 
 /**
  * @brief Allocates memory and panics on allocation error
- * 
+ *
  * @param size Size in bytes to allocate
  * @return void* Pointer to allocated memory
  */
 static void *xmalloc(size_t size)
 {
 	void *ptr;
-	if (size == 0) {
+	if (size == 0)
+	{
 		panic("Size is 0!\n");
 	}
 	ptr = malloc(size);
-	if (!ptr) {
+	if (!ptr)
+	{
 		panic("No mem left!\n");
 	}
 	return ptr;
@@ -36,32 +38,38 @@ static void *xmalloc(size_t size)
 
 /**
  * @brief Retrieves a substring of a string
- * 
+ *
  * @param str The string to retrieve a substring from.
  * @param pos The zero-based starting character position of a substring in str.
  * @param len The number of characters in the substring.
  * @return char* A string that is equivalent to the substring of length len that begins at pos in str.
  */
 
-size_t length_of_string(char chars[]){
-    size_t counter = 0;
-    while(chars[counter] != 0){
-        ++counter;
-    }
-    return counter;
+size_t length_of_string(char chars[])
+{
+	size_t counter = 0;
+	while (chars[counter] != 0)
+	{
+		++counter;
+	}
+	return counter;
 }
 
-static char* substring(const char *str, size_t pos, size_t len)
+static char *substring(const char *str, size_t pos, size_t len)
 {
 	size_t str_length = length_of_string(str);
-	if (pos+len > str_length){
-		printf("ERROR: Size of string is %u. Can't create substring until index %u\n", str_length, pos+len);
+	if (pos + len > str_length)
+	{
+		printf("ERROR: Size of string is %u. Can't create substring until index %u\n", str_length, pos + len);
 		return NULL;
 	}
-	char* sub_str = malloc(sizeof(char) * len);
-	for(size_t i = 0; i < len;++i){
+	char *sub_str = malloc(sizeof(char) * (len + 1));
+	for (size_t i = 0; i < len; ++i)
+	{
 		sub_str[i] = str[pos + i];
 	}
+	sub_str[pos + len] = '\0';
+
 	return sub_str;
 }
 
@@ -69,7 +77,12 @@ int main(int argc, char **argv)
 {
 	char *foo = "Nicht\n";
 	char *bar = substring(foo, 2, 3);
-	if (bar != NULL) printf("%s\n", bar);
+	if (bar != NULL)
+		printf("%s\n", bar);
+
+	printf("length_of_string(foo)=%i", length_of_string(foo));
+	printf("length_of_string(bar)=%i", length_of_string(bar));
+
 	free(bar);
 	return 0;
 }
