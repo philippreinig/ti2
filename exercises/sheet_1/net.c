6cfce786fa54dd9eecbcd4c0be521430850614d3
philippreinig <philipp.reinig@gmail.com>
2022-04-29 23:12:21 +0200

    Corrections sheet_1

--- exercises/sheet_1/net.c
+++ exercises/sheet_1/net.c
@@ -1,21 +1,25 @@
 /* gcc -Wall net.c */
 
 #include <stdio.h>
-#include <stdint.h> 
-
-// unsigned int int_to_bin (unsigned int x) {
-//     return (x == 0 || x == 1 ? x : (x % 2 + 10 * int_to_bin(x / 2)));
-// }
+#include <stdint.h>
 
 int main(void)
 {
-	uint8_t add[] = {129, 169,  25, 100};
-	uint8_t sub[] = {255, 255, 240,   0};
+	uint8_t add[] = {129, 169, 25, 100, 54};
+	uint8_t sub[] = {255, 255, 240, 0};
 	uint8_t net[4];
 
 	/* ... */
 
-	for(int i = 0; i < 4; ++i){
+	printf("sizeof(add)=%lu\n", sizeof(add));
+	printf("sizeof(sub)=%lu\n", sizeof(sub));
+	printf("sizeof(net)=%lu\n", sizeof(net));
+	printf("sizeof(add[0])=%lu\n", sizeof(add[0]));
+	printf("sizeof(sub[0])=%lu\n", sizeof(sub[0]));
+	printf("sizeof(net[0]=%lu\n", sizeof(net[0]));
+
+	for (int i = 0; i < sizeof(add) / sizeof(add[0]); ++i)
+	{
 		net[i] = add[i] & sub[i];
 		printf("%u: %u\t&\t%u\t=\t%u\n", i, add[i], sub[i], net[i]);
 	}
@@ -24,3 +28,7 @@ int main(void)
 
 	return 0;
 }
+
+// unsigned int int_to_bin (unsigned int x) {
+//     return (x == 0 || x == 1 ? x : (x % 2 + 10 * int_to_bin(x / 2)));
+// }
